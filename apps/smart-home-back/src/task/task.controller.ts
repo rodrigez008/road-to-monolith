6519cffb666f47bd0462f6c05279e18d212fd03e
rodrigez008 <rodrigez008@proton.me>
2025-11-19 19:50:35 +0500

    US154 task module

---
+++ apps/smart-home-back/src/task/task.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get('all')
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get('by-id/:id')
+  findById(@Param('id') id: string) {
+    return this.taskService.findById(Number(id));
+  }
+
+  @Post()
+  create() {
+    return this.taskService.create();
+  }
+}
---
+++ apps/smart-home-back/src/task/task.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class TaskService {
+  private tasks = [
+    {
+      id: 1,
+      title: 'sdf',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: '213213',
+      isCompleted: true,
+    },
+  ];
+
+  findAll() {
+    return this.tasks;
+  }
+
+  findById(id: number) {
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return task;
+  }
+
+  create() {
+    const newTask = {
+      id: this.tasks.length + 1,
+      title: 'New task',
+      isCompleted: false,
+    };
+
+    this.tasks.push(newTask);
+
+    return true;
+  }
+}
