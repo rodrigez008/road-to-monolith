cb63c55b3f727add0bc4efcc8c82b74d2ad5cc8a
rodrigez008 <rodrigez008@proton.me>
2025-12-01 19:15:39 +0500

    _US430_ g res

--- apps/cubos-backend/src/app.module.ts
+++ apps/cubos-backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { IS_DEV_ENV } from './libs/common/utils/is-dev';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { PrismaModule } from './prisma/prisma.module';
       ignoreEnvFile: !IS_DEV_ENV,
     }),
     PrismaModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
