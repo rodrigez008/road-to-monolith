381b0aab147b419f9b992f36a9e3a53c210c93db
rodrigez008 <rodrigez008@proton.me>
2025-12-01 19:22:59 +0500

    _US430_ user service

--- apps/cubos-backend/src/user/user.service.ts
+++ apps/cubos-backend/src/user/user.service.ts
@@ -1,7 +1,64 @@
 import { PrismaService } from '@/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { AuthMethod } from '__generated__/prisma/enums';
+import { hash } from 'argon2';
 
 @Injectable()
 export class UserService {
   public constructor(private readonly prismaService: PrismaService) {}
+
+  public async findById(id: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        accounts: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('не найден пользователь');
+    }
+
+    return user;
+  }
+
+  public async findByEmail(email: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+      include: {
+        accounts: true,
+      },
+    });
+
+    return user;
+  }
+
+  public async create(
+    email: string,
+    password: string,
+    displayName: string,
+    picture: string,
+    method: AuthMethod,
+    isVerified: boolean,
+  ) {
+    const user = this.prismaService.user.create({
+      data: {
+        email,
+        password: password ? await hash(password) : '',
+        displayName,
+        picture,
+        method,
+        isVerified,
+      },
+      include: {
+        accounts: true,
+      },
+    });
+
+    return user;
+  }
 }
