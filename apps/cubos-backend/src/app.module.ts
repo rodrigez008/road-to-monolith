91765fd87501b2edcd0b9adeeb87fb630d1aa776
rodrigez008 <rodrigez008@proton.me>
2025-12-01 18:13:46 +0500

    _US430_  utils

--- apps/cubos-backend/src/app.module.ts
+++ apps/cubos-backend/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { IS_DEV_ENV } from './libs/common/utils/is-dev';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: !IS_DEV_ENV,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
